79f030cc8a61567189442d0f3514eee3b86f09cb
chauky <32781912+chauky@users.noreply.github.com>
2018-02-03 16:36:30 -0600

    update restaurantlist

--- client/src/app/admin/restaurantlist.component.ts
+++ client/src/app/admin/restaurantlist.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restaurantlist.component.css']
 })
 export class RestaurantlistComponent implements OnInit {
-
+  public restaurantList=[
+    {id: 1, name: "BurgerKing", address: "12544 15s street", dishes:["Burger", "Coke"], rate: "good" },
+    {id: 2, name: "Pizza Hutt", address: "23 15s street", dishes:["Pizza", "Coke"], rate: "good" },
+    {id: 3, name: "Taco", address: "d 15s street", dishes:["Taco", "Coke"], rate: "good" },
+    {id: 4, name: "McDonal", address: "232 15s street", dishes:["Taco", "Coke"], rate: "good" }
+  ]
   constructor() { }
 
   ngOnInit() {
