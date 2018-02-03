41f79125a5c805af1e10893d0e372a2218f18543
annguyen <tanguyen@mum.edu>
2018-02-03 10:35:36 -0600

    project skeleton

---
+++ server/models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+var userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    username: String,
+    password: String,
+    role: String,
+    created_at: Date,
+    updated_at: Date
+})
+
+module.exports = mongoose.model('User', userSchema);
---
+++ server/services/user.service.js
@@ -0,0 +1,24 @@
+﻿const User = require('../models/user');
+
+var service = {};
+module.exports = service;
+
+function getAll() {
+    
+}
+
+function getById(_id) {
+    
+}
+
+function create(userParam) {
+    
+}
+
+function update(_id, userParam) {
+
+}
+
+function _delete(_id) {
+    
+}
