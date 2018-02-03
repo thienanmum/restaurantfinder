db084cb27d4fe4e8d8daf6dadf1c948c91ec589c
chauky <32781912+chauky@users.noreply.github.com>
2018-02-03 16:07:04 -0600

    Add validate for formControl

--- client/src/app/admin/restaurantdetail.component.ts
+++ client/src/app/admin/restaurantdetail.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+  FormArray,
+  ReactiveFormsModule
+} from "@angular/forms";
+import { Observable } from "rxjs/Rx";
 
 @Component({
   selector: 'app-restaurantdetail',
@@ -6,8 +15,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restaurantdetail.component.css']
 })
 export class RestaurantdetailComponent implements OnInit {
+  myForm: FormGroup;
 
-  constructor() { }
+
+  constructor(private formBuilder: FormBuilder) {
+    this.myForm = formBuilder.group({
+      'name': new FormControl('', [Validators.required]),
+      'street': new FormControl('', [Validators.required]),
+      'city': new FormControl('', [Validators.required]),
+      'state': new FormControl('', [Validators.required]),
+      'zip': new FormControl('', [Validators.required]),
+      'detail': new FormControl('', [Validators.required])
+
+    });
+  };
 
   ngOnInit() {
   }
