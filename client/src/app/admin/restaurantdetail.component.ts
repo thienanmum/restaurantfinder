f2ba331c3159f232e57f10c6dcddae52fe0e24ca
chauky <32781912+chauky@users.noreply.github.com>
2018-02-03 15:27:26 -0600

    First design UI for Admin with CRUD restaurant

---
+++ client/src/app/admin/restaurantdetail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurantdetail',
+  templateUrl: './restaurantdetail.component.html',
+  styleUrls: ['./restaurantdetail.component.css']
+})
+export class RestaurantdetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ client/src/app/admin/restaurantlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurantlist',
+  templateUrl: './restaurantlist.component.html',
+  styleUrls: ['./restaurantlist.component.css']
+})
+export class RestaurantlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
