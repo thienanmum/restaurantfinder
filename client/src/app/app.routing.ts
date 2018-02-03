ea346896ee41e048238f11ae217bda7c1baad799
DESKTOP-UQMD7D2\Quynh Nhu <trannuquynhnhu@gmail.com>
2018-02-03 13:53:20 -0600

    css for searching page

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -1,10 +1,12 @@
 import {Routes, RouterModule} from '@angular/router'
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
+import { RestaurantsearchComponent } from './restaurantsearch/restaurantsearch.component';
 
 const MAIN_ROUTES: Routes = [
     {path:"", redirectTo:'home', pathMatch:'full'},
     {path:"home", component:HomeComponent},
+    {path:"search", component: RestaurantsearchComponent},
     {path:"user/:id?", component:UserComponent}
 ]
 
