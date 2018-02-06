c432d37b13b5ce5e460852e55dbcce6189d9ebbf
chauky <32781912+chauky@users.noreply.github.com>
2018-02-05 20:15:25 -0600

    add author and comments

--- client/src/app/admin/restaurantdetail.component.ts
+++ client/src/app/admin/restaurantdetail.component.ts
@@ -1,6 +1,12 @@
-// Author: ChauKy Nguyen
-// ID: 986085
-// Decription: component for details of Restaurant
+/**
+ * Filename: c:\Users\dnam8\Desktop\project\trunk\client\src\app\admin\restaurantdetail.component.ts
+ * Path: c:\Users\dnam8\Desktop\project\trunk\client
+ * Created Date: Monday, February 5th 2018, 7:56:30 pm
+ * Author: KyNguyen
+ * 
+ * Copyright (c) 2018 Your Company
+ */
+
 
 import { Component, OnInit } from '@angular/core';
 import {
@@ -14,13 +20,14 @@ import {
 import { Observable } from "rxjs/Rx";
 import { RestaurantService } from '../_services/restaurant.service';
 import { Router, ActivatedRoute } from '@angular/router';
-import {Subscription} from "rxjs/Rx";
+import { Subscription } from "rxjs/Rx";
 
 @Component({
   selector: 'app-restaurantdetail',
   templateUrl: './restaurantdetail.component.html',
   styleUrls: ['./restaurantdetail.component.css']
 })
+
 export class RestaurantdetailComponent implements OnInit {
   myForm: FormGroup;
   private subscription: Subscription;
@@ -28,6 +35,7 @@ export class RestaurantdetailComponent implements OnInit {
   private restaurantDetail;
 
   constructor(private formBuilder: FormBuilder, private restaurantService: RestaurantService, private router: Router, private route: ActivatedRoute) {
+    // initial form with empty string
     this.myForm = formBuilder.group({
       'name': new FormControl('', [Validators.required]),
       'address': new FormGroup({
@@ -38,73 +46,70 @@ export class RestaurantdetailComponent implements OnInit {
       }),
       'dishes': new FormControl('', [Validators.required])
     });
-
-   
   };
 
 
-// Save/update restaurant detail from FORM
+  // Save/update restaurant detail from FORM
   save() {
-   
+    // split dish as seperated by comma and trim space 
     var dishes: String = this.myForm.value['dishes'];
-    if(dishes.indexOf(",") !== -1){
+    if (dishes.indexOf(",") !== -1) {
       dishes = this.myForm.value['dishes'].split(",").map(function (item) {
-      return item.trim();
-    });;
+        return item.trim();
+      });;
     }
     this.myForm.value['dishes'] = dishes;
-     if(this.id == 0){
-        this.restaurantService.addRestaurant(this.myForm.value)
-        .subscribe(data => {console.log("add new restaurant"); this.router.navigate(['restaurant'])}, err => console.log(err));
-     }else{
-        this.restaurantService.updateRestaurant(this.id, this.myForm.value)
-        .subscribe(data => {console.log("update the old restaurant"); this.router.navigate(['restaurant'])}, err => console.log(err));
-     }
+    //if the id in URL is equal  it meant add new restaurant
+    if (this.id == 0) {
+      this.restaurantService.addRestaurant(this.myForm.value)
+        .subscribe(data => { console.log("add new restaurant"); this.router.navigate(['restaurant']) }, err => console.log(err));
+    // if the id is not equal 0 it mean update the restaurant
+    } else {
+      this.restaurantService.updateRestaurant(this.id, this.myForm.value)
+        .subscribe(data => { console.log("update the old restaurant"); this.router.navigate(['restaurant']) }, err => console.log(err));
+    }
   }
 
   // Load Restaurant detail into Form
   ngOnInit() {
-     this.subscription = this.route.params.subscribe((param:any) => {
-     this.id = param['id'];
-     if(this.id !=="0"){
-     console.log(this.id)
-      this.restaurantService.getRestaurantById(this.id).subscribe(
-      data=>{
-        console.log("return data");
-        this.restaurantDetail = data; 
-        err => console.log(err);
-       //load FORM value
-        this.myForm.setValue({
-          name: this.restaurantDetail.name,
-          address: {
-            street: this.restaurantDetail.address.street,
-            city: this.restaurantDetail.address.city,
-            state: this.restaurantDetail.address.state,
-            zip: this.restaurantDetail.address.zip
-          },
-          dishes: this.restaurantDetail.dishes
+    this.subscription = this.route.params.subscribe((param: any) => {
+      this.id = param['id'];
+
+      //load the restaurant detail into form, the id !=0 meant load existed restaurant info
+      if (this.id !== "0") {
+        this.restaurantService.getRestaurantById(this.id).subscribe(
+          data => {
+            this.restaurantDetail = data;
+            err => console.log(err);
+            //set FORM value
+            this.myForm.setValue({
+              name: this.restaurantDetail.name,
+              address: {
+                street: this.restaurantDetail.address.street,
+                city: this.restaurantDetail.address.city,
+                state: this.restaurantDetail.address.state,
+                zip: this.restaurantDetail.address.zip
+              },
+              dishes: this.restaurantDetail.dishes
+            });
           });
-    });
-   }
-    });
-    
-  }
-
-
-//delete the restaurant
-delete(){
-  if(this.id !=="0"){
-     console.log(this.id);
-     this.restaurantService.deleteRestaurantById(this.id).subscribe(
-      data=>{
-        console.log("Deleted restaurant with ID" + this.id);
-        this.router.navigate(['restaurant'])
+      }
     });
   }
-}
 
-// destroy subscription to release memory
-  inOnDestroy(){
+
+  //delete the restaurant by id
+  delete() {
+    if (this.id !== "0") {
+      this.restaurantService.deleteRestaurantById(this.id).subscribe(
+        data => {
+          this.router.navigate(['restaurant'])
+        });
+    }
+  }
+
+  // destroy subscription to release memory
+  inOnDestroy() {
     this.subscription.unsubscribe();
   }
 
--- server/services/restaurant.service.js
+++ server/services/restaurant.service.js
@@ -7,22 +7,28 @@ var mongoose = require("mongoose");
 var Restaurant = require("../models/restaurant");
 
 var service = {};
+//get all restaurants from mongodb
 service.getAllRestaurants = function(){    
     return Restaurant.find({});    
 }
 
+//get restaurant by  Id from mongodb
 service.getRestaurantById = function(id){    
     return Restaurant.findById(id);    
 }
+
+//add new restaurant from mongodb
 service.addNewRestaurant = function(restaurant){ 
     restaurant._id = null;
     return Restaurant.create(restaurant);    
 }
 
+//delete the restaurant by query id from mongodb
 service.deleteRestaurantById = function(id){ 
     return Restaurant.findByIdAndRemove(id);    
 }
 
+//update the restaurant by Id from mongodb
 service.updateRestaurantById = function(id, restaurant){ 
     return Restaurant.findByIdAndUpdate(id, restaurant);    
 }
