30363a28c534cf6aedf283f049184212c6114a7b
Quynh Nhu <trannuquynhnhu@gmail.com>
2018-02-06 11:25:31 -0600

    Fix bug for address of restaurant and display the images

--- server/controllers/restaurantview.controller.js
+++ server/controllers/restaurantview.controller.js
@@ -4,11 +4,11 @@ var fs = require("fs");
 
 router.get("/images/:filename", readImage);
 
-function readImage(req, res){   
-    console.log(req.params.filename);
-    
-    //fs.readFile('./public/images/google.png', (err, data) => {
-    fs.readFile('./public/images/' + req.params.filename, (err, data) => {
+function readImage(req, res){       
+   
+    var fileName = req.params.filename + "_1.jpg";
+    fs.readFile('./public/images/' + fileName, (err, data) => {
+        console.log(fileName);
         if(err) throw err;        
         res.writeHead(200, {'Content-Type': 'image/jpeg'});
         res.end(data); 
